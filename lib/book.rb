64bd5da5e81c8a92be5457b85d7aef968ba7588d
Pam Cairo <pcairo_90@outlook.com>
2020-01-08 23:46:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,3 +12,23 @@ class Book
 
 end
 
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  # creates the writer for genre and adds the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+end
